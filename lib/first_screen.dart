848d555df06248ef683cad4113955099219cdeec
shinjizen <rhadityahdw3@gmail.com>
2024-09-25 09:38:54 +0700

    committed

---
+++ lib/first_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Lorem Ipsum')),
+    );
+  }
+}
